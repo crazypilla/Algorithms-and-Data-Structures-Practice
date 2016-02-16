423155fc74adf101149dba7bc76db7928310c3b5
Harshita Vallurupalli <hershey2703@gmail.com>
2016-02-16 00:27:24 -0500

    Create roundrobin.java

---
+++ roundrobin.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package roundrobin;
+
+/**
+ *
+ * @author Harshita V
+ */
+public class Roundrobin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n=4,remain=n;
+        int i=0;int time;
+        for(time=0,i=0;remain!=0;)
+        {
+         System.out.println("heyyy"+i);
+         i++;
+         
+        
+        
+        }
+        
+        }
+        
+    }
+    
+}
